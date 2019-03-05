8dcf7c52cf6d086a4f7c19b7d0714e11faf89517
frankdb <fd.barros@yahoo.com>
2019-03-05 10:36:05 -0800

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This project was built for San Diego Code School. It calculates the change due in dollar and coin denominations. It uses the Materialize styling library.
+
+To view the website, open the index.html file in a browser.
--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-// Write your JavaScript here
+$(document).ready(function() {
+  $("#calculate-change").click(e => {
+    e.preventDefault();
+    const amountDue = Number($("#amount-due").val());
+    const amountReceived = Number($("#amount-received").val());
+    handleCalculateChange(amountDue, amountReceived);
+  });
+});
+
+const handleCalculateChange = (cost, payment) => {
+  $("#dollars-output").empty();
+  $("#quarters-output").empty();
+  $("#dimes-output").empty();
+  $("#nickels-output").empty();
+  $("#pennies-output").empty();
+  // $("#amount-due").val("");
+  // $("#amount-received").val("");
+
+  let change = (payment - cost) * 1000;
+
+  if (change < 0) {
+    alert("Insufficient Funds");
+  }
+
+  const coinTypes = ["dollars", "quarters", "dimes", "nickels", "pennies"];
+  const coinValues = [1000, 250, 100, 50, 10];
+
+  var amount;
+
+  for (var i = 0; i < coinValues.length; i++) {
+    amount = Math.floor(change / coinValues[i]);
+    if (amount > 0) {
+      $(`#${coinTypes[i]}-output`).append(`${amount}`);
+      change = change % coinValues[i];
+    } else {
+      $(`#${coinTypes[i]}-output`).append("0");
+    }
+  }
+};
